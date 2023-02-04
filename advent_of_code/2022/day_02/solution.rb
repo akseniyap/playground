88f714d6852e70e23641a3d54be9705a8d20fcc7
Akseniya Penkova <akseniya.penkova@gmail.com>
2023-02-04 17:29:37 +0200

    Ruby solution day#02, 2022

---
+++ advent_of_code/2022/day_02/solution.rb
@@ -0,0 +1,54 @@
+require_relative "../utils/ruby_utils/constants"
+
+
+def raw_data(variation)
+  File.open("inputs/02_#{variation}.txt").read
+end
+
+def modify(data)
+  data.split(NEW_LINE)
+      .map { |line| line.split }
+end
+
+MOVE_SCORE = {"X" => 1, "Y" => 2, "Z" => 3}
+OUTCOME = {
+    "AX" => 3, "AY" => 6, "AZ" => 0,
+    "BX" => 0, "BY" => 3, "BZ" => 6,
+    "CX" => 6, "CY" => 0, "CZ" => 3
+}
+OPTION_FOR_OUTCOME = {
+    "AX" => "Z", "AY" => "X", "AZ" => "Y",
+    "BX" => "X", "BY" => "Y", "BZ" => "Z",
+    "CX" => "Y", "CY" => "Z", "CZ" => "X"
+}
+
+def easy(data)
+  score = 0
+  moves = modify(data)
+
+  moves.each do |his, my|
+    score += OUTCOME[his + my]
+    score += MOVE_SCORE[my]
+  end
+
+  score
+end
+
+def hard(data)
+  score = 0
+  moves = modify(data)
+
+  moves.each do |his, needed_outcome|
+    my = OPTION_FOR_OUTCOME[his + needed_outcome]
+    score += OUTCOME[his + my]
+    score += MOVE_SCORE[my]
+  end
+
+  score
+end
+
+if __FILE__ == $0
+  data = raw_data(INPUT)
+  puts easy(data)
+  puts hard(data)
+end
