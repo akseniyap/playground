3cd4f515fd3df43dfa533335e6f19b4a8207aea8
Akseniya Penkova <akseniya.penkova@gmail.com>
2023-02-04 16:22:32 +0200

    Python solution day#02, 2022

---
+++ advent_of_code/2022/day_02/solution.py
@@ -0,0 +1,48 @@
+from utils.py_utils.constants import INPUT, NEW_LINE, SPACE
+from utils.py_utils.decorators import split_data, split_elements_by
+
+
+@split_elements_by(SPACE)
+@split_data(NEW_LINE)
+def get_data(variation):
+    with open(f"inputs/02_{variation}.txt") as f:
+        data = f.read()
+    return data
+
+
+MOVE_SCORE = {"X": 1, "Y": 2, "Z": 3}
+OUTCOME = {
+    "AX": 3, "AY": 6, "AZ": 0,
+    "BX": 0, "BY": 3, "BZ": 6,
+    "CX": 6, "CY": 0, "CZ": 3
+}
+OPTION_FOR_OUTCOME = {
+    "AX": "Z", "AY": "X", "AZ": "Y",
+    "BX": "X", "BY": "Y", "BZ": "Z",
+    "CX": "Y", "CY": "Z", "CZ": "X"
+}
+
+
+def easy(data):
+    score = 0
+    for his, my in data:
+        score += OUTCOME[his + my]
+        score += MOVE_SCORE[my]
+
+    return score
+
+
+def hard(data):
+    score = 0
+    for his, needed_outcome in data:
+        my = OPTION_FOR_OUTCOME[his + needed_outcome]
+        score += OUTCOME[his + my]
+        score += MOVE_SCORE[my]
+
+    return score
+
+
+if __name__ == "__main__":
+    data = list(get_data(INPUT))
+    print(easy(data))
+    print(hard(data))
---
+++ advent_of_code/2022/day_02/test.py
@@ -0,0 +1,20 @@
+import unittest
+from solution import easy, hard, get_data
+from utils.py_utils.constants import SAMPLE
+
+
+class Day02(unittest.TestCase):
+    def setUp(self):
+        self.data = get_data(SAMPLE)
+
+    def test_easy(self):
+        result = easy(self.data)
+        self.assertEqual(result, 15)
+
+    def test_hard(self):
+        result = hard(self.data)
+        self.assertEqual(result, 12)
+
+
+if __name__ == "__main__":
+    unittest.main()
